7a4579a3619dc158e2452a99b2a9ee0b0d71ffa7
aw663 <48367469+aw663@users.noreply.github.com>
2021-09-12 22:09:25 -0400

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@ ESP32WebServer server(80);
 #include "esp_wps.h"
 //#include "OTAServer.h"
 #include "handleSerial.h"
+#include "analogReadFunction.h"
 //#include <ArduinoOTA.h> //added by ardy
 
 void setup() {
@@ -146,28 +147,3 @@ void loop() {
       }
 
     }
-
-//*******************************************//
-//******** Self-made functions **************//
-//*******************************************//
-
-//******** Function to read multiple values and average them **************//
-// inputs: All the specific values are initialized in the beginning
-// 1. dly_avg (in us) = Duration of the delay per detector read.
-// 2. n_reads         = number of times detectors are being read. e.g. n_reads = 5 --> the detetctors is averaging between 5 values
-// 3. sensorPin       = Which pin are you reading. As each time this function is called, different pin inputs are inserted (in the main loop)
-
-int analogReadFunction(const int dly_avg, const int n_reads, const int sensorPin)
-{
-  int sensorValue = 0;
-  for (int i = 0; i <= n_reads; i++)
-  {
-    delayMicroseconds(dly_avg);
-    if (i >= 1)
-    {
-      sensorValue = analogRead(sensorPin) + sensorValue;
-    }
-    //sensor1 = far; sensor2 = near
-  }
-  return sensorValue / (n_reads);
-}
